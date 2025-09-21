730e069e5ebb91d3089a112e229796568209ce76
Francisco-Alfredo <sanumbofrancisco@gmail.com>
2025-09-21 13:28:05 +0100

    Primeiro commit

---
+++ utils.py
@@ -0,0 +1,47 @@
+from dataset import df
+import pandas as pd
+import streamlit as st
+import time
+
+def format_number(value, prefix = ''):
+    for unit in ['', 'mil']:
+        if value < 1000:
+            return f'{prefix} {value:.2f} {unit}'
+        value /=1000
+    return f'{prefix} {value:.2f} milhões'
+
+#1- DataFrame Receita por Estado
+df_receita_estado = df.groupby('Local da compra')[['Preço']].sum()
+df_receita_estado = df.drop_duplicates(subset='Local da compra')[['Local da compra', 'lat', 'lon']].merge(
+df_receita_estado, left_on='Local da compra', 
+    right_index=True ).sort_values('Preço', ascending=False)
+
+#print(def_receita_estado)
+
+#2- DataFrame Receita Mensal
+df_receita_mensal = df.set_index('Data da Compra').groupby(pd.Grouper(freq='M'))['Preço'].sum().reset_index()
+df_receita_mensal['Ano'] = df_receita_mensal['Data da Compra'].dt.year
+df_receita_mensal['Mes'] = df_receita_mensal['Data da Compra'].dt.month_name()
+
+#print(df_receita_mensal)
+
+#3 - DataFrame Receita por Categoria
+df_receita_categoria =df.groupby('Categoria do Produto')[['Preço']].sum().sort_values('Preço', ascending=False)
+
+#print(df_receita_categoria.head())
+
+#4 - DataFrame Vendedores
+df_vendedores = pd.DataFrame(df.groupby('Vendedor')['Preço'].agg(['sum', 'count']))
+#print(df_vendedores)
+
+#5 - Função para converter arquivo csv
+@st.cache_data
+def convert_csv(df):
+    return df.to_csv(index=False).encode('utf-8')
+
+def mensagem_sucesso():
+    success = st.success('Arquivo baixado com sucesso',
+                         icon='✅')
+    time.sleep(3)
+    success.empty()
+
